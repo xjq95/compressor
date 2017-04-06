07c89b5d62743368f23b1444805358076b63c269
yibanguaiqi <jiaqi7282545@gmail.com>
2017-04-06 20:36:33 +0800

    修改 jp2 jpeg 压缩

--- src/main/java/me/fjnu/compressor/process/Jp2Process.java
+++ src/main/java/me/fjnu/compressor/process/Jp2Process.java
@@ -2,61 +2,58 @@ package me.fjnu.compressor.process;
 
 import com.github.jaiimageio.jpeg2000.J2KImageWriteParam;
 import me.fjnu.compressor.domain.CompressInfo;
-import me.fjnu.compressor.exception.InvalidInputEncodedDataFileException;
-import me.fjnu.compressor.exception.InvalidParamsException;
 import me.fjnu.compressor.util.MyByteUtil;
 import org.apache.commons.io.FileUtils;
 import org.springframework.stereotype.Component;
 
 import javax.imageio.IIOImage;
 import javax.imageio.ImageIO;
-import javax.imageio.ImageWriteParam;
 import javax.imageio.ImageWriter;
 import javax.imageio.stream.ImageOutputStream;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
-import java.util.Iterator;
 import java.util.regex.Pattern;
 import java.util.zip.DataFormatException;
 
 /**
- * Created by xujiaqi on 17.3.31.
+ * Created by xujiaqi on 17.3.30.
  * todo
  */
 @Component
-public class Jp2LossyProcess implements CompressProcess {
+public class Jp2Process implements CompressProcess {
 	@Override
-	public byte[] compress(byte[] srcBytes, CompressInfo compressInfo) throws IOException, InvalidParamsException {
-		J2KImageWriteParam iwp = new J2KImageWriteParam();
+	public byte[] compress(byte[] srcBytes, CompressInfo compressInfo) throws IOException {
+		ImageWriter writer = ImageIO.getImageWritersByFormatName("JPEG2000").next();
+		System.out.println(writer.getClass().getName());
+		J2KImageWriteParam j2kParams = (J2KImageWriteParam) writer.getDefaultWriteParam();
+		j2kParams.setSOP(true);
+		j2kParams.setWriteCodeStreamOnly(true);
+		j2kParams.setProgressionType("layer");
+		j2kParams.setLossless(false);
+		j2kParams.setCompressionMode(J2KImageWriteParam.MODE_EXPLICIT);
+		j2kParams.setCompressionType("JPEG2000");
+		j2kParams.setCompressionQuality(0.1f);
+		j2kParams.setEncodingRate(1.01);
+		j2kParams.setFilter(J2KImageWriteParam.FILTER_97);
 		ByteArrayInputStream bais = new ByteArrayInputStream(srcBytes);
 		BufferedImage image = ImageIO.read(bais);
 		bais.close();
 		
-		Iterator<ImageWriter> writers = ImageIO.getImageWritersBySuffix("jp2");
-		ImageWriter writer = writers.next();
-		J2KImageWriteParam writeParams = (J2KImageWriteParam) writer.getDefaultWriteParam();
-		writeParams.setLossless(false);
-		writeParams.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
-		writeParams.setCompressionType("JPEG2000");
-		writeParams.setCompressionQuality(0.0f);
-		writeParams.setEncodingRate(0.5f);
-		
 		//压缩
-		File f = File.createTempFile("test-jpeg2000-lossless", ".jp2");
+		File f = File.createTempFile("test-jpeg", ".jpeg");
 		f.deleteOnExit();
 		ImageOutputStream ios = ImageIO.createImageOutputStream(f);
 		writer.setOutput(ios);
-		writer.write(null, new IIOImage(image, null, null), writeParams);
+		writer.write(null, new IIOImage(image, null, null), j2kParams);
 		writer.dispose();
-		
 		return FileUtils.readFileToByteArray(f);
 	}
 	
 	@Override
-	public byte[] uncompress(
-			byte[] bytes, CompressInfo compressInfo) throws DataFormatException, IOException, InvalidParamsException, InvalidInputEncodedDataFileException {
+	public byte[] uncompress(byte[] bytes, CompressInfo compressInfo) throws DataFormatException, IOException {
+		
 		BufferedImage bufferedImage = MyByteUtil.createImageFromBytes(bytes);
 		String[] s = compressInfo.getMultipartFile().getOriginalFilename().split(Pattern.quote("."));
 		String suffix = s[s.length - 2];
--- src/main/java/me/fjnu/compressor/process/JpegProcess.java
+++ src/main/java/me/fjnu/compressor/process/JpegProcess.java
@@ -2,9 +2,18 @@ package me.fjnu.compressor.process;
 
 import me.fjnu.compressor.domain.CompressInfo;
 import me.fjnu.compressor.util.MyByteUtil;
+import org.apache.commons.io.FileUtils;
 import org.springframework.stereotype.Component;
 
+import javax.imageio.IIOImage;
+import javax.imageio.ImageIO;
+import javax.imageio.ImageWriteParam;
+import javax.imageio.ImageWriter;
+import javax.imageio.plugins.jpeg.JPEGImageWriteParam;
+import javax.imageio.stream.ImageOutputStream;
 import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.File;
 import java.io.IOException;
 import java.util.regex.Pattern;
 import java.util.zip.DataFormatException;
@@ -17,8 +26,24 @@ import java.util.zip.DataFormatException;
 public class JpegProcess implements CompressProcess {
 	@Override
 	public byte[] compress(byte[] srcBytes, CompressInfo compressInfo) throws IOException {
-		BufferedImage bufferedImage = MyByteUtil.createImageFromBytes(srcBytes);
-		return MyByteUtil.writeBytes(bufferedImage, "jpeg");
+		ImageWriter writer = ImageIO.getImageWritersByFormatName("jpeg").next();
+		System.out.println(writer.getClass().getName());
+		JPEGImageWriteParam jpegParams = (JPEGImageWriteParam) writer.getDefaultWriteParam();
+		jpegParams.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
+		jpegParams.setCompressionQuality(1.0f);
+		
+		ByteArrayInputStream bais = new ByteArrayInputStream(srcBytes);
+		BufferedImage image = ImageIO.read(bais);
+		bais.close();
+		
+		//压缩
+		File f = File.createTempFile("test-jpeg", ".jpeg");
+		f.deleteOnExit();
+		ImageOutputStream ios = ImageIO.createImageOutputStream(f);
+		writer.setOutput(ios);
+		writer.write(null, new IIOImage(image, null, null), jpegParams);
+		writer.dispose();
+		return FileUtils.readFileToByteArray(f);
 	}
 	
 	@Override
